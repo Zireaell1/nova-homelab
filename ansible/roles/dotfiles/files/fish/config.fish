a284bf0e085b5fb5e47a2bcd6fd14ae747239d4f
Zireaell1 <51866727+Zireaell1@users.noreply.github.com>
2026-08-05 23:40:24 +0200

    feat: add command list to fish_greeting

--- ansible/roles/dotfiles/files/fish/config.fish
+++ ansible/roles/dotfiles/files/fish/config.fish
@@ -7,4 +7,17 @@ end
 
 function fish_greeting
     fastfetch
+
+    echo ""
+    set_color -o blue
+    echo "Quick commands:"
+    set_color green
+    echo -n "  up      "
+    set_color normal
+    echo "-> update packages (dnf upgrade)"
+    set_color green
+    echo -n "  upclean "
+    set_color normal
+    echo "-> update and clean up (dnf autoremove)"
+    echo ""
 end
